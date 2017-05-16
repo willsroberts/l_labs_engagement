a72fdac4c824b45edb56ea813fb2bfd69505b469
Will Roberts <wsroberts@gmail.com>
2017-05-16 08:22:00 -0700

    grid search included

--- src/engage_churn/model/grid_search_model.py
+++ src/engage_churn/model/grid_search_model.py
@@ -6,18 +6,18 @@ def grid_search_dict():
     defines all the models and hyperparameters to optimize with grid search.
     returns a list of tuples - (model, dict-of-hyper_params)
     '''
-    #
-    # gd_boost = {
-    #     'learning_rate':[1, 0.05],
-    #     'max_depth':[2,4],
-    #     'max_features':['sqrt', 'log2'],
-    #     'n_estimators':[50, 100]}
-    #
-    # ada_boost = {
-    #     'learning_rate':[1, 0.05],
-    #     'base_estimator__max_depth':[2,4],
-    #     'base_estimator__max_features':['sqrt', 'log2'],
-    #     'n_estimators':[50, 100]}
+    
+    gd_boost = {
+        'learning_rate':[1, 0.05],
+        'max_depth':[2,4],
+        'max_features':['sqrt', 'log2'],
+        'n_estimators':[50, 100]}
+
+    ada_boost = {
+        'learning_rate':[1, 0.05],
+        'base_estimator__max_depth':[2,4],
+        'base_estimator__max_features':['sqrt', 'log2'],
+        'n_estimators':[50, 100]}
 
     random_forest_grid = {
         'n_estimators': [50],
@@ -26,7 +26,7 @@ def grid_search_dict():
         }
 
     return [
-        # (GradientBoostingClassifier(), gd_boost),
-        # (AdaBoostClassifier(DecisionTreeClassifier()), ada_boost),
+        (GradientBoostingClassifier(), gd_boost),
+        (AdaBoostClassifier(DecisionTreeClassifier()), ada_boost),
         (RandomForestClassifier(), random_forest_grid)
     ]
