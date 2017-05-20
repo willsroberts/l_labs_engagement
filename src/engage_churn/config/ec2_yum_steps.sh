13e9fd965b77e6b2e01f9f57277c5ca89cc24df1
Will Roberts <wsroberts@gmail.com>
2017-05-19 18:34:33 -0700

    shell steps - not script yet - needed for amazon linux setup

---
+++ src/engage_churn/config/ec2_yum_steps.sh
@@ -0,0 +1,14 @@
+`
+sudo yum install git
+sudo yum install pip
+sudo pip install --upgrade pip
+sudo yum install gcc
+sudo su -
+yum install python-devel
+yum install libevent-devel
+pip install ipython
+yum install python-dev
+pip install ipython
+pip install pandas
+
+`
